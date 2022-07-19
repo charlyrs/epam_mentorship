659ffd38b3d8c732781369bc8998289148807a26
Liliya <charlyrsmail@gmail.com>
2022-07-19 12:34:32 +0300

    watering plants

--- LeetCode/AddTwoNumbers.py
+++ LeetCode/AddTwoNumbers.py
@@ -1,9 +1,5 @@
-# Definition for singly-linked list.
 # https://leetcode.com/problems/add-two-numbers/
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         left = 0
